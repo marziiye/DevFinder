0e84de07483a659f310b434ebff89d6635288cb3
mrz.sf <marziye.seifi.b@gmail.com>
2022-12-12 15:48:10 +0330

    Seprating signals - create signal.py- and modifying post and delete post signals

--- users/models.py
+++ users/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -35,14 +32,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
-
-@receiver(post_save, sender = Profile)
-def CrerateProfile(sender, instance, created, **kwargs):
-    print('Profile saved!')
-    print('Instance:', instance)
-    print('Created:', created)
-
-
-@receiver(post_delete, sender=Profile)
-def deleteUser(sender, instance, **kwargs):
-    print('Deleting user...')
